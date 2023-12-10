bbcbb3c0865282eac3987c584c1e311c6d025b59
Roland Ernotte <rol@rern.be>
2023-12-10 21:28:22 +0100

    step 3

--- www/js/index.js
+++ www/js/index.js
@@ -18,16 +18,19 @@ function onDeviceReady() {
 
     var mainView = f7App.views.create('.view-main');
 
-    // variable globale: réponse de l'API poiSearch
+    // variables globales
+    // réponse de l'API poiSearch
     var storedData;
+    // latitude et longitude
+    var latitude, longitude;
 
     // on géolocalise l'appareil
     navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
 
     // callback en cas de succès de géolocalisation
     function geolocationSuccess(position){
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
         
         // on fait disparaitre le texte 'searching'
         $('#searching').text('');
@@ -35,7 +38,7 @@ function onDeviceReady() {
         // point terminal de l'API poiSearch de Tomtom
         var apiUrl = 'https://api.tomtom.com/search/2/poiSearch/pizza.json?lat='
                       + latitude + '&lon=' + longitude
-                      + '&key=TOMTOM_KEY';
+                      + '&key=SDxukVh1qhhHWpqMypU2G10gbnNbJjdi';
         
         // requête asynchrone vers le point terminal de l'API Tomtom
         $.getJSON(apiUrl, function(data){
@@ -137,6 +140,69 @@ function onDeviceReady() {
 		// on l'injecte dans un élément de class tel de la apge de détail
         $('.page[data-name="details"] .page-content .tel').text(tel);
 
+
+    })
+
+    // pour savoir si le tab a déjà été affiché
+    var tabMapNeverDisplayed = true;
+
+    // lorsque le tab-map est chargé...
+    // (voir http://framework7.io/docs/tabs.html#tabs-events)
+    $('#tab-map').on('tab:show', function(){
+        // console.log('coucou');
+        
+        // si la latitude et la longitude de l'utilisateur ont été récupérées
+        if(latitude != undefined && longitude != undefined && tabMapNeverDisplayed){
+            
+            tabMapNeverDisplayed = false;
+
+            // on initialise la carte avec un centre et un niveau de zoom
+            var map = L.map('mapid').setView([latitude, longitude], 13);
+
+            // on affiche une couche de tuiles (qui proviennent de openstreetmap) sur la carte
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            // on ajoute le marqueur qui donne notre position
+            L.marker([latitude, longitude]).addTo(map)
+                                            .bindPopup('Votre position')
+                                            .openPopup();
+
+            // icône pour marqueur vert (bleu par défaut)
+            var greenIcon = new L.Icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+                });
+            
+            // on ajoute les marqueurs des pizzérias
+            $.each(storedData.results, function(){
+                // console.log(this.position.lat);
+                var lat = this.position.lat;
+                var lon = this.position.lon;
+                var name = this.poi.name;
+                L.marker([lat, lon], {icon: greenIcon}).addTo(map)
+                                    .bindPopup('<a class="popupLink" href="#" data-id="' + this.id + '">' + this.poi.name + '</a>');
+            })
+        }  
+    })
+
+    // on détecte le click sur un lien dans un popup de la carte
+    $(document).on('click', '.popupLink', function(){
+        // console.log('popup link clicked');
+        // console.log($(this).attr('data-id'));
+
+        var id = $(this).attr('data-id');
+
+        // on navigue vers la page de détail
+		mainView.router.navigate({
+            name: 'details',
+            params: {id: id}
+        });
 
     })
     
